8725062c01659e28d09a18285d1dbca602edbfcd
Chris Day <c_day@alumni.baylor.edu>
2018-05-16 10:48:42 -0500

    added page title to projects page

--- server.js
+++ server.js
@@ -56,7 +56,9 @@ app.get('/bad', (req, res) => {
 });
 
 app.get('/projects', (req, res) => {
-  res.render('projects.hbs');
+  res.render('projects.hbs', {
+    pageTitle: 'Projects'
+  });
 });
 
 
